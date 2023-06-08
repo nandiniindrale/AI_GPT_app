3e307dbca7fa0d1c6058f6f4da86658c6e717cc3
Nandu1907 <nandiniindrale2002@gmail.com>
2023-06-08 19:58:28 +0530

    add some pages

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react'
-import {Box,Link,Typography ,useTheme} from '@mui/material'
+import {Box,Typography ,useTheme} from '@mui/material'
+import axios from "axios";
+import toast from "react-hot-toast";
+import { NavLink } from "react-router-dom";
+
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const theme = useTheme();
+    const navigate = useNavigate();
+    const loggedIn=JSON.parse(localStorage.getItem('authToken'))
+
+    
+  //handle logout
+  const handleLogout = async () => {
+    try {
+      await axios.post("http://localhost:5000/api/v1/auth/logout");
+      localStorage.removeItem("authToken");
+      toast.success("logout successfully ");
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Box
       width={"100%"}
@@ -14,8 +35,26 @@ const Navbar = () => {
      <Typography variant="h1" color="primary" fontWeight="bold">
         AI GPT3 Clone
      </Typography>
-     <Link href="/register" p={1}>Sign Up</Link>
-     <Link href="/login" p={1}>Sign In </Link>
+     {loggedIn ? (
+        <>
+          <NavLink to="/" p={1}>
+            Home
+          </NavLink>
+          <NavLink to="/login" onClick={handleLogout} p={1}>
+            Logout
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink to="/register" p={1}>
+            Sign Up
+          </NavLink>
+          <NavLink to="/login" p={1}>
+            Sign In
+          </NavLink>
+        </>
+      )}
+
     </Box >
   )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -28,7 +28,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/v1/auth/register", { username, email, password });
+     await axios.post("http://localhost:5000/api/v1/auth/register", { username, email, password });
       toast.success("User Register Successfully");
       navigate("/login");
     } catch (err) {
